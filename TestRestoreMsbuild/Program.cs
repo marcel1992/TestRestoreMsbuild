38e25592f6717ec22c3f9f7b6856ec0e0c41e61d
www.valachmarcel@gmail.com <marcel.valach@yahoo.ro>
2020-09-24 23:08:28 +0300

    Add project files.

---
+++ TestRestoreMsbuild/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestRestoreMsbuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
